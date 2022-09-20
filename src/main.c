f7452a6f2b271c93799fc3f6fc6b5f5cca613aed
esettes <ioanaroxana.st@gmail.com>
2022-09-20 20:41:05 +0200

    update mac settings

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: iostancu <iostancu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 21:31:37 by iostancu          #+#    #+#             */
-/*   Updated: 2022/09/12 19:59:02 by iostancu         ###   ########.fr       */
+/*   Updated: 2022/09/20 20:30:18 by iostancu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	ft_leaks()
 int	main(int argc, char	*argv[])
 {
 	t_fdf	*fdf;
-	//atexit(&ft_leaks);
+	atexit(&ft_leaks);
 	if (argc < 3)
 	{
 		fdf = (t_fdf *)malloc(sizeof(t_fdf));
